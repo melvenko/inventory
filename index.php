8db11f61c8420132a5c180bebba6791870db35d4
melvenko <54460468+melvenko@users.noreply.github.com>
2025-02-27 06:47:29 +0800

    column right: align: justify

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
             border-radius: 10px;
             box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
             display: flex;
-            align-items: center;
+            align-items: justify;
             justify-content: center;
             min-height: 300px;
         }
